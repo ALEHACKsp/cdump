915a6c6601f4523e547a1707f99f284a8ce15634
coltonon <cnpearson42@gmail.com>
2018-11-13 03:55:32 -0600

    first commit

---
+++ cdump/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace cdump
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.processView = new System.Windows.Forms.ListView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dumpButton = new System.Windows.Forms.Button();
+            this.processBox = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.processView, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.statusLabel, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(678, 391);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // processView
+            // 
+            this.processView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.processView.FullRowSelect = true;
+            this.processView.GridLines = true;
+            this.processView.Location = new System.Drawing.Point(3, 63);
+            this.processView.Name = "processView";
+            this.processView.Size = new System.Drawing.Size(672, 325);
+            this.processView.TabIndex = 0;
+            this.processView.UseCompatibleStateImageBehavior = false;
+            this.processView.View = System.Windows.Forms.View.Details;
+            this.processView.ItemActivate += new System.EventHandler(this.processView_ItemActivate);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.checkBox1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dumpButton, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.processBox, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 23);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(672, 34);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoCheck = false;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Location = new System.Drawing.Point(573, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 28);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dumpButton
+            // 
+            this.dumpButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dumpButton.Enabled = false;
+            this.dumpButton.Location = new System.Drawing.Point(594, 3);
+            this.dumpButton.Name = "dumpButton";
+            this.dumpButton.Size = new System.Drawing.Size(75, 28);
+            this.dumpButton.TabIndex = 1;
+            this.dumpButton.Text = "Dump";
+            this.dumpButton.UseVisualStyleBackColor = true;
+            this.dumpButton.Click += new System.EventHandler(this.dumpButton_Click);
+            // 
+            // processBox
+            // 
+            this.processBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.processBox.Location = new System.Drawing.Point(3, 7);
+            this.processBox.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.processBox.Name = "processBox";
+            this.processBox.Size = new System.Drawing.Size(564, 20);
+            this.processBox.TabIndex = 2;
+            this.processBox.TextChanged += new System.EventHandler(this.processBox_TextChanged);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusLabel.Location = new System.Drawing.Point(3, 7);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(672, 13);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Select A Process";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 391);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CDump";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView processView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button dumpButton;
+        private System.Windows.Forms.TextBox processBox;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ cdump/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace cdump
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("pd.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        static extern string doShit(string exe, string path);
+
+        public Form1()
+        {
+            InitializeComponent();
+            processView.Columns.Add("Process Name", -1, HorizontalAlignment.Left);
+            processView.Columns.Add("Window Name", -2, HorizontalAlignment.Right);
+            RefreshProcesses();
+        }
+
+        void RefreshProcesses()
+        {
+            string search = processBox.Text;
+            processView.Items.Clear();
+            foreach (Process process in Process.GetProcesses())
+            {
+                try
+                {
+                    if (process.MainWindowTitle.Contains(search) || process.MainModule.ModuleName.Contains(search))
+                    {
+                        var item1 = new ListViewItem(new[] {process.MainModule.ModuleName, process.MainWindowTitle});
+                        processView.Items.Add(item1);
+                    }
+                } catch (Exception) { }
+            }
+            this.ResizeColumnHeaders();
+            CheckProcess();
+        }
+
+        void CheckProcess()
+        {
+            
+            this.checkBox1.Checked = this.dumpButton.Enabled = (processBox.Text.Length > 4) && (Process.GetProcessesByName(processBox.Text.Substring(0, processBox.Text.Length-4)).Length > 0);
+            processBox.BackColor = (this.checkBox1.Checked) ? Color.LightGreen : Color.LightCoral;
+            statusLabel.Text = (this.checkBox1.Checked) ? "Ready to Dump" : "Select a Process";
+        }
+
+        private void processBox_TextChanged(object sender, EventArgs e)
+        {
+            RefreshProcesses();
+        }
+
+        private void processView_ItemActivate(object sender, EventArgs e)
+        {
+            if (processView.SelectedItems.Count == 1)
+            {
+                processBox.Text = processView.SelectedItems[0].Text;
+            }
+        }
+
+        private void ResizeColumnHeaders()
+        {
+            for (int i = 0; i < this.processView.Columns.Count - 1; i++) this.processView.AutoResizeColumn(i, ColumnHeaderAutoResizeStyle.ColumnContent);
+            this.processView.Columns[this.processView.Columns.Count - 1].Width = -2;
+        }
+
+        public void InvokeUI(Action a)
+        {
+            this.BeginInvoke(new MethodInvoker(a));
+        }
+
+        void doDump(string procName)
+        {
+            if (!File.Exists("pd.dll"))
+            {
+                File.WriteAllBytes("pd.dll", cdump.Properties.Resources.pd);
+            }
+
+            string currentPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string dumpPath = currentPath + @"\dumps";
+            string dumpProcPath = dumpPath + @"\" + procName.Substring(0, procName.Length - 4);
+            string dumpPathFull = dumpProcPath + @"\" + DateTime.Now.ToString("yyyy-M-dd HH-mm-ss");
+
+            if (!Directory.Exists(dumpPath))
+            {
+                Directory.CreateDirectory(dumpPath);
+            }
+            if (!Directory.Exists(dumpProcPath))
+            {
+                Directory.CreateDirectory(dumpProcPath);
+            }
+
+            if (!Directory.Exists(dumpPathFull))
+            {
+                Directory.CreateDirectory(dumpPathFull);
+            }
+
+            doShit(procName, dumpPathFull);
+            InvokeUI(() => {
+                dumpButton.Enabled = processBox.Enabled = processView.Enabled = true;
+                statusLabel.Text = "Dump Complete";
+                processBox.BackColor = Color.LightGreen;
+            });
+        }
+
+        private void dumpButton_Click(object sender, EventArgs e)
+        {
+            dumpButton.Enabled = processBox.Enabled = processView.Enabled = false;
+            processBox.BackColor = Color.LightBlue;
+            statusLabel.Text = "Dumping, please wait . . .";
+            Thread thread = new Thread(() => doDump(processBox.Text));
+            thread.Start();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.ResizeColumnHeaders();
+        }
+    }
+}
